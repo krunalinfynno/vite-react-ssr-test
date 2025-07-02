8b65e56ffdccbf4c14fed74441b3c6d40e6bf691
Krunal Shah <shahkrunal7091@gmail.com>
2025-07-02 17:41:39 +0530

    fix: vercel deploy

---
+++ startServer.js
@@ -0,0 +1,8 @@
+import { app } from "./server.js";
+
+const port = process.env.PORT || 5173;
+
+// Start http server
+app.listen(port, () => {
+  console.log(`Server started at http://localhost:${port}`);
+});
